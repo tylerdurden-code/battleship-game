65723bd124bf150134bb4333364ab4f8935233a2
Tyler Durden <tylerdurden1412@gmail.com>
2022-07-25 00:51:30 +0300

    finished webpack

--- webpack.common.js
+++ webpack.common.js
@@ -9,10 +9,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
         test: /\.html$/,
         use: ['html-loader'],
       },
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -10,5 +12,21 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: './imgs/[name].[hash].[ext]',
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css',
+  }), new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
 });
