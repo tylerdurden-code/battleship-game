fda2867e0406325896f414912559a3aeca5a85a7
Tyler Durden <tylerdurden1412@gmail.com>
2022-07-28 21:45:47 +0300

    drag and drop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 
 const sum = require('./contructors/object');
+const dragAndDrop = require('./draganddrop');
 
 console.log('webpack genius');
 console.log(sum(5, 10));
 console.log(sum(5, 10));
+dragAndDrop();
