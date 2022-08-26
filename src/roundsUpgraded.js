bf7fa4b87f133fb4e4134627e092599889af6ccc
Tyler Durden <tylerdurden1412@gmail.com>
2022-08-26 23:22:45 +0300

    add dom manipulation

--- src/roundsUpgraded.js
+++ src/roundsUpgraded.js
@@ -1,3 +1,43 @@
+const generateRandomInteger = require('./randomNum');
+
+function isWinner(ships) {
+  let variable = true;
+  ships.forEach((ship) => {
+    if (ship.isItSunk === false) {
+      variable = false;
+    }
+  });
+  return variable;
+}
+
+function hitUpgraded(x, y, playerObject, gameboard, ships, box = null) {
+  if (gameboard[x][y] === null) {
+    if (box === null) {
+      return;
+    }
+    box.classList.add('hitButMissed');
+    console.log('you missed');
+  }
+  if (gameboard[x][y] !== null) {
+    ships.forEach((ship) => {
+      if (ship.name === gameboard[x][y]) {
+        ship.hit();
+        ship.isSunk();
+        console.log(ship);
+      }
+      if (box === null) {
+        return;
+      }
+      box.classList.add('hitSuccess');
+    });
+    gameboard[x][y] = null;
+    console.log(playerObject);
+    gameboard.forEach((row) => {
+      console.log(row);
+    });
+  }
+}
+
 function roundsUpgraded(playerList, gameboardPlayer1, gameboardPlayer2, shipsP1, shipsP2) {
   const heroBoard = document.querySelector('.heroBoard');
   const enemyBoard = document.querySelector('.enemyBoard');
@@ -20,11 +60,59 @@ function roundsUpgraded(playerList, gameboardPlayer1, gameboardPlayer2, shipsP1,
   boxesArr.forEach((box) => {
     box.addEventListener('click', () => {
       if (box.parentNode.parentNode.classList.contains('active')) {
-        console.log('big man');
-        enemyBoard.classList.toggle('inactive');
-        enemyBoard.classList.toggle('active');
-        heroBoard.classList.toggle('inactive');
-        heroBoard.classList.toggle('active');
+        if (rotationTip.textContent === `${playerList[0].name} choose hit coordinates`) {
+          if (beingHitCoordsP2.includes(`x=${box.dataset.rowNum} y=${box.dataset.colNum}`)) {
+            return;
+          }
+          hitUpgraded(
+            box.dataset.rowNum,
+            box.dataset.colNum,
+            playerList[1],
+            gameboardPlayer2,
+            shipsP2,
+            box,
+          );
+          if (isWinner(shipsP2)) {
+            console.log(`${playerList[0].name} won`);
+          }
+          beingHitCoordsP2.push(`x=${box.dataset.rowNum} y=${box.dataset.colNum}`);
+          console.log(beingHitCoordsP2);
+
+          let checker = false;
+          let x = generateRandomInteger(9);
+          let y = generateRandomInteger(9);
+          while (!checker) {
+            if (beingHitCoordsP1.includes(`x=${x} y=${y}`)) {
+              x = generateRandomInteger(9);
+              y = generateRandomInteger(9);
+            } else {
+              hitUpgraded(
+                x,
+                y,
+                playerList[0],
+                gameboardPlayer1,
+                shipsP1,
+              );
+              beingHitCoordsP1.push(`x=${x} y=${y}`);
+              checker = true;
+              if (isWinner(shipsP1)) {
+                console.log(`${playerList[0].name} won`);
+              }
+
+              console.log(beingHitCoordsP1);
+            }
+          }
+        }
+
+        // enemyBoard.classList.toggle('inactive');
+        // enemyBoard.classList.toggle('active');
+        // heroBoard.classList.toggle('inactive');
+        // heroBoard.classList.toggle('active');
+        // if (rotationTip.textContent === `${playerList[0].name} choose hit coordinates`) {
+        //   rotationTip.textContent = `${playerList[1].name} choose hit coordinates`;
+        // } else {
+        //   rotationTip.textContent = `${playerList[0].name} choose hit coordinates`;
+        // }
       }
     });
   });
